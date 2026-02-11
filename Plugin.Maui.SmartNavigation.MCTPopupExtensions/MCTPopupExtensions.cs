3373dd77d76bf4f14b60e5b407ba8780bbd28b6e
Kyle Cuthbert <Kyle.Cuthbert@cardiff.gov.uk>
2026-02-11 14:35:00 +0000

    Add MCT popup extensions and generalize page resolution
    Added Plugin.Maui.SmartNavigation.MCTPopupExtensions project with extension methods for CommunityToolkit.Maui popups, enabling DI-based popup resolution and navigation. Refactored NavigationExtensions to generalize page resolution from Page to IView for broader compatibility. Updated solution and project files to include and expose internals to the new extension project.

---
+++ Plugin.Maui.SmartNavigation.MCTPopupExtensions/MCTPopupExtensions.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Maui;
+using CommunityToolkit.Maui.Views;
+using Plugin.Maui.SmartNavigation.Extensions;
+using Plugin.Maui.SmartNavigation.Services;
+
+namespace Plugin.Maui.SmartNavigation;
+
+public static class MCTPopupExtensions
+{
+    public static async Task PushAsync<T>(this IPopupService navigation) where T: Popup
+    {
+        var popup = Resolver.Resolve<T>() as Popup
+            ?? throw new ArgumentException("Could not resolve popup page");
+
+        await navigation.PushAsync(popup, true);
+    }
+
+    public static async Task PushAsync<T>(this IPopupService navigation, params object[] parameters) where T : Popup
+    {
+        var popup = NavigationExtensions.ResolvePage<T>(parameters) as Popup
+            ?? throw new ArgumentException("Could not resolve popup page");
+
+        await navigation.PushAsync(popup, true);
+    }
+}
